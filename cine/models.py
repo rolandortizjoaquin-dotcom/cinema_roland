818c8cd35487da88d47a2132d39eba0413e0e817
Roland Gyver <rolandgyver@gmail.com>
2026-02-23 20:49:44 -0500

    cambios para mi 3er reforzamiento

--- cine/models.py
+++ cine/models.py
@@ -24,3 +24,11 @@ class Entrada(models.Model):
 
     def __str__(self):
         return self.codigo
+
+
+class Snack(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
